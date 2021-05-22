6fc73779959e1c35cf747f9a9c10e6fefab10d0a
Yang Hanlin <73731881+Lynino@users.noreply.github.com>
2021-05-22 19:24:45 +0800

    Delete bata.py

--- bata.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-
-
-a = torch.tensor([[1,1,1],[2,2,2]], dtype=torch.float32)
-b = torch.tensor([[1,2,3],[4,5,6]], dtype=torch.float32)
-c = torch.mul(a,b)
-print(c)
